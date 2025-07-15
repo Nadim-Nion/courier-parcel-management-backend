502be2603af56097df20dc5a99fefcbf7ced0f84
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-07-15 21:25:06 +0600

    vercel issues solved

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,7 @@ const app: Application = express();
 
 // Parser or Middleware
 app.use(express.json());
-app.use(
-  cors({
-    origin: ['http://localhost:5173'],
-  }),
-);
+app.use(cors({ origin: ['http://localhost:5173'] }));
 
 // Application Route
 app.use('/api/v1', router);
