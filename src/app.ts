cda669b4152181293a6f9dfedc0bb889bc11158e
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-08-03 22:41:29 +0600

    cors origin again modified

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:5173',
-      'https://courier-parcel-frontend.netlify.app/',
+      'https://courier-parcel-frontend.netlify.app',
     ],
   }),
 );
